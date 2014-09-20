9817e10063d128c71c6724b9d13a13a712431b48
Anitha Rao <annasaru@gmail.com>
2014-09-20 18:22:48 -0400

    added controller page

---
+++ script.js
@@ -0,0 +1,17 @@
+var myapp = angular.module('myapp', []);
+myapp.controller('myCtrl', function ($scope){	
+	$scope.data = {
+					personname:'person name',
+		        	jobtitle: 'job title',
+		        	tedioustask: 'tedious task',
+		            malename: 'male name',
+		        	dirtytask: 'dirty task',
+		        	celebrity: 'celebrity',
+		        	uselessskill: 'useless skill',
+		        	adjective: 'adjective',
+		        	obnoxiouscelebrity: 'obnoxious celebrity',
+		        	hugenumber: 'huge number'
+		    		};
+
+});
+	
